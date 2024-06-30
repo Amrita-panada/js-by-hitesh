f5e4326cf8adddd22815837a1cb7a127ec6c1fae
Amrita-panada <154084480+Amrita-panada@users.noreply.github.com>
2024-06-30 14:21:29 +0000

    new code updated

---
+++ basics/dataType_2.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)// we are using nodejs,not browser
+
+//ECMA,MDN,TC39
+
+let name ="amrita"
+let age = 18
+let isloggedIn= false
+
+// number +> 2 to power 53
+// bigint
+// string=>""
+// null => standalone value ,it is a object
+// undefined=> where value not defined
+// boolean => true or false
+// symbol => unique
+
+//object
+
+console.log(typeof age );
+console.log(typeof null );
+console.log(typeof undefined );
---
+++ basics/variables_1.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "panda.amrita6@gmail.com"
+var accountPassword="123432"
+let accountState;
+
+// accountId=232
+accountEmail="amita@gam.com"
+accountPassword = "221212"
+
+console.log(accountId);
+
+/*
+do not use var
+because of issue in block scope and funtionnal scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountState]);
